2a35130ac55408686477384983c4e60a945f3f18
ChecheRomo96 <josemauel.romo.96@gmail.com>
2021-08-30 02:40:17 -0500

    .

---
+++ examples/BuildTest/BuildTest.ino
@@ -0,0 +1,24 @@
+#include <MCC.h>
+
+using namespace MCC_MusicalNote;
+
+void setup()
+{
+
+    Serial.begin(115200);
+
+    Serial.print(F("This project uses CPString version:"));
+    Serial.println(CPSTRING_VERSION);
+
+    Serial.print(F("This project uses CPVector version:"));
+    Serial.println(CPVECTOR_VERSION);
+
+    Serial.print(F("This project uses MCC version:"));
+    Serial.println(MCC_VERSION);
+
+}
+
+void loop()
+{
+
+}
---
+++ examples/BuildTest/CPP/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <CPString.h>
+#include <CPVector.h>
+#include <MCC.h>
+
+int main()
+{
+    std::cout<<"This project uses CPString version: "<<CPSTRING_VERSION<<std::endl;
+    std::cout<<"This project uses CPVector version: "<<CPVECTOR_VERSION<<std::endl;
+    std::cout<<"This project uses MCC version: "<<MCC_VERSION<<std::endl;
+	return 0;
+}
