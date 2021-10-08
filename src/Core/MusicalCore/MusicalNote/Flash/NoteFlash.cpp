4377dcc76797989cc87767102ed11fc7b07de995
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2021-10-08 16:25:04 -0500

    Update NoteFlash.cpp

--- src/Core/MusicalCore/MusicalNote/Flash/NoteFlash.cpp
+++ src/Core/MusicalCore/MusicalNote/Flash/NoteFlash.cpp
@@ -108,8 +108,8 @@ char Flash::Buffer[24];
 
 	char* Flash::GetName(char* buff, const Pitch::LetterType& Letter, const Pitch::AccidentalType& Accidental, int8_t Octave, const TextFormat::FormatClass& Format)
 	{
-		Serial.println("Checkpoint A");
-		
+		Serial.println("Checkpoint A");delay(1000);
+
 		if((Letter == Pitch::Letter::Count)||(Accidental < Pitch::Accidental::Min)||(Accidental > Pitch::Accidental::Max)){buff[0] = '\0'; return buff;}
 	    
 		CPString::string LetterStr(Flash::GetLetterText(Letter));
@@ -119,7 +119,7 @@ char Flash::Buffer[24];
 
 		CPString::string OutputStr;
 
-		Serial.println("Checkpoint B");
+		Serial.println("Checkpoint B");delay(1000);
 
 
 		switch(Format.SpacingMode())
