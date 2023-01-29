597c1a1737164eccdf0548378a022796fd3e5300
José <josemanuel.romo.96@gmail.com>
2023-01-29 03:21:31 -0600

    Update MidiMessage.cpp

--- src/Core/MidiCore/MidiMessage/MidiMessage.cpp
+++ src/Core/MidiCore/MidiMessage/MidiMessage.cpp
@@ -2,7 +2,7 @@
 #include "../MidiDefinitions.h"
 #include "../../MusicalCore/MusicalCore.h"
 
-using namespace MusicompositionCore::Core::MidiCore;
+using namespace MusiCompositionCore::Core::MidiCore;
 
 MidiMessage::MidiMessage()
 {
