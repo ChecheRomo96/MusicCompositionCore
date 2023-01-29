e147e8d870dcdd9e32a986a6a336d3695029c5c9
José <josemanuel.romo.96@gmail.com>
2023-01-29 03:05:17 -0600

    .

--- src/Core/MidiCore/MidiMessage/MidiMessage.cpp
+++ src/Core/MidiCore/MidiMessage/MidiMessage.cpp
@@ -5,7 +5,7 @@
 
 MidiMessage::MidiMessage()
 {
-
+	return *this;
 }
 
 MidiMessage& MidiMessage::operator=(const MidiNote& Source)
@@ -16,36 +16,44 @@ MidiMessage& MidiMessage::operator=(const MidiNote& Source)
 	{
 		Buffer[i] = Source.Buffer[i];
 	}
+
+	return *this;
 }
 
-void MidiMessage::NoteOn(uint8_t Pitch, uint8_t Vel , uint8_t Channel = 0)
+MidiMessage& MidiMessage::NoteOn(uint8_t Pitch, uint8_t Vel , uint8_t Channel = 0)
 {
 	Buffer.Resize(3);
 
 	Buffer[0] = MCC_MidiProtocol::NoteOn & (Channel&=0x0F);
 	Buffer[1] = Pitch;
 	Buffer[2] = Vel;
+
+	return *this;
 }
 
-void MidiMessage::NoteOn(const MidiCore::MidiNote& Source)
+MidiMessage& MidiMessage::NoteOn(const MidiCore::MidiNote& Source)
 {
 	Buffer.Resize(3);
 
 	Buffer[0] = MCC_MidiProtocol::NoteOn & (Channel&=0x0F);
 	Buffer[1] = Source.Pitch();
 	Buffer[2] = Source.Velocity();
+	
+	return *this;
 }
 
-void MidiMessage::NoteOn(const MusicalCore::MusicalNote::Note& Source, uint8_t Vel, uint8_t Channel = 0)
+MidiMessage& MidiMessage::NoteOn(const MusicalCore::MusicalNote::Note& Source, uint8_t Vel, uint8_t Channel = 0)
 {
 	Buffer.Resize(3);
 
 	Buffer[0] = MCC_MidiProtocol::NoteOn & (Channel&=0x0F);
 	Buffer[1] = Source.MidiPitch();
 	Buffer[2] = Vel;
+	
+	return *this;
 }
 
-void MidiMessage::NoteOn(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Octave, uint8_t Vel, uint8_t Channel = 0)
+MidiMessage& MidiMessage::NoteOn(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Octave, uint8_t Vel, uint8_t Channel = 0)
 {
 	Buffer.Resize(3);
 	MCC_MusicalNote::Note tmpNote(Source,Octave);
@@ -53,36 +61,44 @@ void MidiMessage::NoteOn(const MusicalCore::MusicalNote::Pitch& Source, uint8_t 
 	Buffer[0] = MCC_MidiProtocol::NoteOn & (Channel&=0x0F);
 	Buffer[1] = tmpNote.MidiPitch();
 	Buffer[2] = Vel;
+	
+	return *this;
 }
 
-void MidiMessage::NoteOff(uint8_t Pitch, uint8_t Channel = 0)
+MidiMessage& MidiMessage::NoteOff(uint8_t Pitch, uint8_t Channel = 0)
 {
 	Buffer.Resize(3);
 
 	Buffer[0] = MCC_MidiProtocol::NoteOff & (Channel&=0x0F);
 	Buffer[1] = Pitch;
 	Buffer[2] = 0;
+	
+	return *this;
 }
 
-void MidiMessage::NoteOff(const MidiCore::MidiNote& Source)
+MidiMessage& MidiMessage::NoteOff(const MidiCore::MidiNote& Source)
 {
 	Buffer.Resize(3);
 
 	Buffer[0] = MCC_MidiProtocol::NoteOff & (Channel&=0x0F);
 	Buffer[1] = Source.Pitch();
 	Buffer[2] = 0;
+	
+	return *this;
 }
 
-void MidiMessage::NoteOff(const MusicalCore::MusicalNote::Note& Source, uint8_t Channel = 0)
+MidiMessage& MidiMessage::NoteOff(const MusicalCore::MusicalNote::Note& Source, uint8_t Channel = 0)
 {
 	Buffer.Resize(3);
 
 	Buffer[0] = MCC_MidiProtocol::NoteOff & (Channel&=0x0F);
 	Buffer[1] = Source.MidiPitch();
 	Buffer[2] = 0;
+	
+	return *this;
 }
 
-void MidiMessage::NoteOff(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Octave, uint8_t Channel = 0)
+MidiMessage& MidiMessage::NoteOff(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Octave, uint8_t Channel = 0)
 {
 	Buffer.Resize(3);
 	MCC_MusicalNote::Note tmpNote(Source,Octave);
@@ -90,56 +106,80 @@ void MidiMessage::NoteOff(const MusicalCore::MusicalNote::Pitch& Source, uint8_t
 	Buffer[0] = MCC_MidiProtocol::NoteOff & (Channel&=0x0F);
 	Buffer[1] = tmpNote.MidiPitch();
 	Buffer[2] = 0;
+	
+	return *this;
 }
 
-void MidiMessage::ProgramChange(uint8_t Program, uint8_t Channel = 0)
+MidiMessage& MidiMessage::ProgramChange(uint8_t Program, uint8_t Channel = 0)
 {
+	Buffer.Resize(2);
 
-}
-void MidiMessage::ContinuousController(uint8_t ControllerNumber, uint8_t Value, uint8_t Channel = 0)
-{
-
-}
-void MidiMessage::PitchBend(int16_t Value, uint8_t Channel = 0)
-{
-
-}
-void MidiMessage::PolyphonicAfetrtouch(uint8_t Note, uint8_t Pressure, uint8_t Channel = 0)
-{
-
+	Buffer[0] = MCC_MidiProtocol::ProgramChange & (Channel&=0x0F);
+	Buffer[1] = Program;
+	
+	return *this;
 }
 
-void MidiMessage::Start()
+MidiMessage& MidiMessage::ContinuousController(uint8_t ControllerNumber, uint8_t Value, uint8_t Channel = 0)
 {
 
+	
+	return *this;
 }
-void MidiMessage::Continue()
+MidiMessage& MidiMessage::PitchBend(int16_t Value, uint8_t Channel = 0)
 {
 
+	
+	return *this;
 }
-void MidiMessage::Stop()
+MidiMessage& MidiMessage::PolyphonicAfetrtouch(uint8_t Note, uint8_t Pressure, uint8_t Channel = 0)
 {
 
-}
-
-void MidiMessage::ClockTick()
-{
-
-}
-void MidiMessage::SongPositionCounter(uint16_t Position)
-{
-
-}
-void MidiMessage::SongSelect(uint8_t Song)
-{
-
+	
+	return *this;
 }
 
-void MidiMessage::SystemExclusive(const CPVector::vector& Data)
+MidiMessage& MidiMessage::Start()
 {
 
+	
+	return *this;
 }
-void MidiMessage::SystemExclusive(uint8_t* Data, uint8_t Length)
+MidiMessage& MidiMessage::Continue()
 {
 
+	
+	return *this;
+}
+MidiMessage& MidiMessage::Stop()
+{
+
+	return *this;
+}
+
+MidiMessage& MidiMessage::ClockTick()
+{
+
+	return *this;
+}
+MidiMessage& MidiMessage::SongPositionCounter(uint16_t Position)
+{
+
+	return *this;
+}
+MidiMessage& MidiMessage::SongSelect(uint8_t Song)
+{
+
+	return *this;
+}
+
+MidiMessage& MidiMessage::SystemExclusive(const CPVector::vector& Data)
+{
+
+	return *this;
+}
+MidiMessage& MidiMessage::SystemExclusive(uint8_t* Data, uint8_t Length)
+{
+	
+	return *this;
 }
--- src/Core/MidiCore/MidiMessage/MidiMessage.h
+++ src/Core/MidiCore/MidiMessage/MidiMessage.h
@@ -22,31 +22,31 @@ namespace MusicCompositionCore
                     MidiMessage& MidiMessage::operator=(const MidiMessage& Source);
                     const CPVector::vector& Buffer
 
-                    void NoteOn(uint8_t Pitch, uint8_t Vel , uint8_t Channel = 0);
-                    void NoteOn(const MidiCore::MidiNote& Source);
-                    void NoteOn(const MusicalCore::MusicalNote::Note& Source, uint8_t Vel, uint8_t Channel = 0);
-                    void NoteOn(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Vel, uint8_t Channel = 0);
+                    MidiMessage& NoteOn(uint8_t Pitch, uint8_t Vel , uint8_t Channel = 0);
+                    MidiMessage& NoteOn(const MidiCore::MidiNote& Source);
+                    MidiMessage& NoteOn(const MusicalCore::MusicalNote::Note& Source, uint8_t Vel, uint8_t Channel = 0);
+                    MidiMessage& NoteOn(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Vel, uint8_t Channel = 0);
 
-                    void NoteOff(uint8_t Pitch, uint8_t Channel = 0);
-                    void NoteOff(const MidiCore::MidiNote& Source);
-                    void NoteOff(const MusicalCore::MusicalNote::Note& Source, uint8_t Channel = 0);
-                    void NoteOff(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Octave, uint8_t Channel = 0);
+                    MidiMessage& NoteOff(uint8_t Pitch, uint8_t Channel = 0);
+                    MidiMessage& NoteOff(const MidiCore::MidiNote& Source);
+                    MidiMessage& NoteOff(const MusicalCore::MusicalNote::Note& Source, uint8_t Channel = 0);
+                    MidiMessage& NoteOff(const MusicalCore::MusicalNote::Pitch& Source, uint8_t Octave, uint8_t Channel = 0);
 
-                    void ProgramChange(uint8_t Program, uint8_t Channel = 0);
-                    void ContinuousController(uint8_t ControllerNumber, uint8_t Value, uint8_t Channel = 0);
-                    void PitchBend(int16_t Value, uint8_t Channel = 0);
-                    void PolyphonicAfetrtouch(uint8_t Note, uint8_t Pressure, uint8_t Channel = 0);
+                    MidiMessage& ProgramChange(uint8_t Program, uint8_t Channel = 0);
+                    MidiMessage& ContinuousController(uint8_t ControllerNumber, uint8_t Value, uint8_t Channel = 0);
+                    MidiMessage& PitchBend(int16_t Value, uint8_t Channel = 0);
+                    MidiMessage& PolyphonicAfetrtouch(uint8_t Note, uint8_t Pressure, uint8_t Channel = 0);
 
-                    void Start();
-                    void Continue();
-                    void Stop();
+                    MidiMessage& Start();
+                    MidiMessage& Continue();
+                    MidiMessage& Stop();
 
-                    void ClockTick();
-                    void SongPositionCounter(uint16_t Position);
-                    void SongSelect(uint8_t Song);
+                    MidiMessage& ClockTick();
+                    MidiMessage& SongPositionCounter(uint16_t Position);
+                    MidiMessage& SongSelect(uint8_t Song);
 
-                    void SystemExclusive(const CPVector::vector& Data);
-                    void SystemExclusive(uint8_t* Data, uint8_t Length);
+                    MidiMessage& SystemExclusive(const CPVector::vector& Data);
+                    MidiMessage& SystemExclusive(uint8_t* Data, uint8_t Length);
 
             };
         }
