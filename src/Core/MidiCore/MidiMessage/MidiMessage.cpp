fe1e76a54dcff9d6f4bf860125dcc93b768f64f7
José <josemanuel.romo.96@gmail.com>
2023-01-31 22:55:09 -0600

    Update MidiMessage.cpp

--- src/Core/MidiCore/MidiMessage/MidiMessage.cpp
+++ src/Core/MidiCore/MidiMessage/MidiMessage.cpp
@@ -30,7 +30,7 @@ MidiMessage& MidiMessage::NoteOn(uint8_t Pitch, uint8_t Vel, uint8_t Channel)
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOn | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOn | (Channel&0x0F);
 	_Buffer[1] = Pitch & 0x7F;
 	_Buffer[2] = Vel & 0x7F;
 
@@ -41,7 +41,7 @@ MidiMessage& MidiMessage::NoteOn(const MidiCore::MidiNote& Source)
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOn | (Source.Channel()&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOn | (Source.Channel()&0x0F);
 	_Buffer[1] = Source.Pitch();
 	_Buffer[2] = Source.Velocity();
 	
@@ -52,7 +52,7 @@ MidiMessage& MidiMessage::NoteOn(const MusicalCore::MusicalNote::Note& Source, u
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOn | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOn | (Channel&0x0F);
 	_Buffer[1] = Source.MidiPitch();
 	_Buffer[2] = Vel & 0x7F;
 	
@@ -64,7 +64,7 @@ MidiMessage& MidiMessage::NoteOn(const MusicalCore::MusicalNote::Pitch& Source, 
 	_Buffer.resize(3);
 	MCC_MusicalNote::Note tmpNote(Source,Octave);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOn | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOn | (Channel&0x0F);
 	_Buffer[1] = tmpNote.MidiPitch();
 	_Buffer[2] = Vel & 0x7F;
 	
@@ -75,7 +75,7 @@ MidiMessage& MidiMessage::NoteOff(uint8_t Pitch, uint8_t Channel)
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOff | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOff | (Channel&0x0F);
 	_Buffer[1] = Pitch & 0x7F;
 	_Buffer[2] = 0;
 	
@@ -86,7 +86,7 @@ MidiMessage& MidiMessage::NoteOff(const MidiCore::MidiNote& Source)
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOff | (Source.Channel()&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOff | (Source.Channel()&0x0F);
 	_Buffer[1] = Source.Pitch();
 	_Buffer[2] = 0;
 	
@@ -97,7 +97,7 @@ MidiMessage& MidiMessage::NoteOff(const MusicalCore::MusicalNote::Note& Source, 
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOff | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOff | (Channel&0x0F);
 	_Buffer[1] = Source.MidiPitch();
 	_Buffer[2] = 0;
 	
@@ -109,7 +109,7 @@ MidiMessage& MidiMessage::NoteOff(const MusicalCore::MusicalNote::Pitch& Source,
 	_Buffer.resize(3);
 	MCC_MusicalNote::Note tmpNote(Source,Octave);
 
-	_Buffer[0] = MCC_MidiProtocol::NoteOff | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::NoteOff | (Channel&0x0F);
 	_Buffer[1] = tmpNote.MidiPitch();
 	_Buffer[2] = 0;
 	
@@ -120,7 +120,7 @@ MidiMessage& MidiMessage::ProgramChange(uint8_t Program, uint8_t Channel)
 {
 	_Buffer.resize(2);
 
-	_Buffer[0] = MCC_MidiProtocol::ProgramChange | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::ProgramChange | (Channel&0x0F);
 	_Buffer[1] = Program & 0x7F;
 	
 	return *this;
@@ -130,7 +130,7 @@ MidiMessage& MidiMessage::ControlChange(uint8_t ControllerNumber, uint8_t Value,
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::ControlChange | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::ControlChange | (Channel&0x0F);
 	_Buffer[1] = ControllerNumber & 0x7F;
 	_Buffer[2] = Value & 0x7F;
 
@@ -141,7 +141,7 @@ MidiMessage& MidiMessage::PitchBend(int16_t Value, uint8_t Channel)
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::PitchBend | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::PitchBend | (Channel&0x0F);
 	_Buffer[1] = Value & 0xFF;
 	_Buffer[2] = (Value>>8) & 0xFF;
 	
@@ -152,7 +152,7 @@ MidiMessage& MidiMessage::AfterTouch(uint8_t Note, uint8_t Pressure, uint8_t Cha
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::AfterTouch | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::AfterTouch | (Channel&0x0F);
 	_Buffer[1] = Note & 0x7F;
 	_Buffer[2] = Pressure & 0x7F;
 
@@ -163,7 +163,7 @@ MidiMessage& MidiMessage::ChannelPressure(uint8_t Pressure, uint8_t Channel)
 {
 	_Buffer.resize(2);
 
-	_Buffer[0] = MCC_MidiProtocol::ChannelPressure | (Channel&0x0F);
+	_Buffer[0] = MCC_MidiProtocol::ChannelVoice::ChannelPressure | (Channel&0x0F);
 	_Buffer[1] = Pressure & 0x7F;
 
 	return *this;
@@ -173,7 +173,7 @@ MidiMessage& MidiMessage::Start()
 {
 	_Buffer.resize(1);
 
-	_Buffer[0] = MCC_MidiProtocol::SystemMessage | MCC_MidiProtocol::Start;
+	_Buffer[0] = MCC_MidiProtocol::System::RealTime::Start;
 
 	return *this;
 }
@@ -182,7 +182,7 @@ MidiMessage& MidiMessage::Continue()
 {
 	_Buffer.resize(1);
 
-	_Buffer[0] = MCC_MidiProtocol::SystemMessage | MCC_MidiProtocol::Continue;
+	_Buffer[0] = MCC_MidiProtocol::System::RealTime::Continue;
 
 	return *this;
 }
@@ -191,7 +191,7 @@ MidiMessage& MidiMessage::Stop()
 {
 	_Buffer.resize(1);
 
-	_Buffer[0] = MCC_MidiProtocol::SystemMessage | MCC_MidiProtocol::Stop;
+	_Buffer[0] = MCC_MidiProtocol::System::RealTime::Stop;
 
 	return *this;
 }
@@ -200,7 +200,7 @@ MidiMessage& MidiMessage::ClockTick()
 {
 	_Buffer.resize(1);
 
-	_Buffer[0] = MCC_MidiProtocol::SystemMessage | MCC_MidiProtocol::TimingTick;
+	_Buffer[0] = MCC_MidiProtocol::System::RealTime::TimingTick;
 
 	return *this;
 }
@@ -209,7 +209,7 @@ MidiMessage& MidiMessage::SongPositionPointer(uint16_t Position)
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::SystemMessage | MCC_MidiProtocol::SongPositionPointer;
+	_Buffer[0] = MCC_MidiProtocol::System::Common::SongPositionPointer;
 	_Buffer[1] = Position&0xFF;
 	_Buffer[2] = (Position>>8)&0xFF;
 
@@ -220,7 +220,7 @@ MidiMessage& MidiMessage::SongSelect(uint8_t Song)
 {
 	_Buffer.resize(3);
 
-	_Buffer[0] = MCC_MidiProtocol::SystemMessage | MCC_MidiProtocol::SongSelect;
+	_Buffer[0] = MCC_MidiProtocol::System::Common::SongSelect;
 	_Buffer[1] = Song & 0x7F;
 
 	return *this;
@@ -232,12 +232,12 @@ MidiMessage& MidiMessage::SystemExclusive(const CPVector::vector<uint8_t>& Data)
 	bool flag1 = 0;
 	bool flag2 = 0;
 
-	if(Data[0] != (MCC_MidiProtocol::SystemMessage|MCC_MidiProtocol::SystemExclusive))
+	if(Data[0] != (MCC_MidiProtocol::System::Exclusive::Start))
 	{
 		counter++;
 		flag1 = 1;
 	}
-	if(Data[Data.size() - 1] != (MCC_MidiProtocol::SystemMessage|MCC_MidiProtocol::SystemExclusiveEnd))
+	if(Data[Data.size() - 1] != (MCC_MidiProtocol::System::Exclusive::End))
 	{
 		counter++;
 		flag2 = 1;
@@ -245,8 +245,8 @@ MidiMessage& MidiMessage::SystemExclusive(const CPVector::vector<uint8_t>& Data)
 
 	_Buffer.resize(Data.size() + counter);
 
-	if(flag1){ _Buffer[0] = MCC_MidiProtocol::SystemMessage|MCC_MidiProtocol::SystemExclusive; }
-	if(flag2){ _Buffer[_Buffer.size() - 1] = MCC_MidiProtocol::SystemMessage|MCC_MidiProtocol::SystemExclusive; }
+	if(flag1){ _Buffer[0] = MCC_MidiProtocol::System::Exclusive::Start; }
+	if(flag2){ _Buffer[_Buffer.size() - 1] = MCC_MidiProtocol::System::Exclusive::End; }
 	
 	for(uint8_t i = flag1; i < _Buffer.size() - flag2; i++)
 	{
