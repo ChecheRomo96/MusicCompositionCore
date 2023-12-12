f27322ec6c05016ebfbaf4c6bad878c42a978bcb
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-11 19:29:49 -0600

    Update MCC_Accumulator.h

--- src/Core/UtilityCore/Accumulator/MCC_Accumulator.h
+++ src/Core/UtilityCore/Accumulator/MCC_Accumulator.h
@@ -55,8 +55,7 @@
                 template <typename DataType, typename Range = int8_t>
                 auto ElementSorting = [](const Element<DataType,Range>& Compare, const Element<DataType,Range>& Pivot)
                 {
-	                if(Compare.Data()<Pivot.Data()){return false;}
-	                else{return true;}
+	                return Compare.Data()>=Pivot.Data();
                 };
 
                 template <typename DataType, typename Range = int8_t>
