0c94c07555957b7ef113ef2edcff223e7bd58caa
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-16 11:10:54 -0600

    Update NoteSorting.h

--- src/Core/MusicalCore/MusicalNote/Sorting/Note/NoteSorting.h
+++ src/Core/MusicalCore/MusicalNote/Sorting/Note/NoteSorting.h
@@ -12,43 +12,43 @@
 
 		const auto Letter_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.Letter() < Pivot.Letter();
-		}
+		};
 
 		const auto Letter_Descending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.Letter() > Pivot.Letter();
-		}
+		};
 		
 		const auto Accidental_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.Accidental() < Pivot.Accidental();
-		}
+		};
 
 		const auto Accidental_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.Accidental() > Pivot.Accidental();
-		}
+		};
 		
 		const auto NotePitch_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.NotePitch() < Pivot.NotePitch();
-		}
+		};
 
 		const auto NotePitch_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.NotePitch() > Pivot.NotePitch();
-		}
+		};
 		
 		const auto MidiPitch_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.MidiPitch() < Pivot.MidiPitch();
-		}
+		};
 
 		const auto MidiPitch_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.MidiPitch() > Pivot.MidiPitch();
-		}
+		};
 		
 		const auto Octave_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.Octave() < Pivot.Octave();
-		}
+		};
 
 		const auto Octave_Ascending = [](const MusicalNote::Note& Element, const MusicalNote::Note& Pivot){
 			return Element.Octave() > Pivot.Octave();
-		}
+		};
 	}
 
 #endif//MCC_NOTE_CLASS_SORTING_H
