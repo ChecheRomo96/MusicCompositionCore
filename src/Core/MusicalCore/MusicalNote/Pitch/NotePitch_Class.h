e69e519bb248f1e372198b787dc2d4635ab89131
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-19 03:17:30 -0600

    Update NotePitch_Class.h

--- src/Core/MusicalCore/MusicalNote/Pitch/NotePitch_Class.h
+++ src/Core/MusicalCore/MusicalNote/Pitch/NotePitch_Class.h
@@ -30,8 +30,8 @@
                                 
                                 typedef uint8_t Type;
 
-                                static Pitch PitchBuffer;
-                                static Type TypeBuffer;
+                                extern static Pitch PitchBuffer;
+                                extern static Type TypeBuffer;
                           
                                 static constexpr Type C_QuadrupleFlat = 0u;
                                 static constexpr Type C_TripleFlat = 1u;
