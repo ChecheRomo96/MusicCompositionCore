4cfc955b1a83793794de47706f507efb393d28aa
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-19 02:52:04 -0600

    Update NotePitch_Class.h

--- src/Core/MusicalCore/MusicalNote/Pitch/NotePitch_Class.h
+++ src/Core/MusicalCore/MusicalNote/Pitch/NotePitch_Class.h
@@ -28,82 +28,82 @@
                         // typrdef and constants declaration
                             public:
                                 
-                                typedef uint8_t PitchType;
+                                typedef uint8_t Type;
 
                                 static Pitch PitchBuffer;
-                                static PitchType PitchTypeBuffer;
+                                static Type TypeBuffer;
                           
-                                static constexpr PitchType C_QuadrupleFlat = 0u;
-                                static constexpr PitchType C_TripleFlat = 1u;
-                                static constexpr PitchType C_DoubleFlat = 2u;
-                                static constexpr PitchType C_Flat = 3u;
-                                static constexpr PitchType C_Natural = 4u;
-                                static constexpr PitchType C_Sharp = 5u;
-                                static constexpr PitchType C_DoubleSharp = 6u;
-                                static constexpr PitchType C_TripleSharp = 7u;
-                                static constexpr PitchType C_QuadrupleSharp = 8u;
+                                static constexpr Type C_QuadrupleFlat = 0u;
+                                static constexpr Type C_TripleFlat = 1u;
+                                static constexpr Type C_DoubleFlat = 2u;
+                                static constexpr Type C_Flat = 3u;
+                                static constexpr Type C_Natural = 4u;
+                                static constexpr Type C_Sharp = 5u;
+                                static constexpr Type C_DoubleSharp = 6u;
+                                static constexpr Type C_TripleSharp = 7u;
+                                static constexpr Type C_QuadrupleSharp = 8u;
 
-                                static constexpr PitchType D_QuadrupleFlat = 9u;
-                                static constexpr PitchType D_TripleFlat = 10u;
-                                static constexpr PitchType D_DoubleFlat = 11u;
-                                static constexpr PitchType D_Flat = 12u;
-                                static constexpr PitchType D_Natural = 13u;
-                                static constexpr PitchType D_Sharp = 14u;
-                                static constexpr PitchType D_DoubleSharp = 15u;
-                                static constexpr PitchType D_TripleSharp = 16u;
-                                static constexpr PitchType D_QuadrupleSharp = 17u;
+                                static constexpr Type D_QuadrupleFlat = 9u;
+                                static constexpr Type D_TripleFlat = 10u;
+                                static constexpr Type D_DoubleFlat = 11u;
+                                static constexpr Type D_Flat = 12u;
+                                static constexpr Type D_Natural = 13u;
+                                static constexpr Type D_Sharp = 14u;
+                                static constexpr Type D_DoubleSharp = 15u;
+                                static constexpr Type D_TripleSharp = 16u;
+                                static constexpr Type D_QuadrupleSharp = 17u;
 
-                                static constexpr PitchType E_QuadrupleFlat = 18u;
-                                static constexpr PitchType E_TripleFlat = 19u;
-                                static constexpr PitchType E_DoubleFlat = 20u;
-                                static constexpr PitchType E_Flat = 21u;
-                                static constexpr PitchType E_Natural = 22u;
-                                static constexpr PitchType E_Sharp = 23u;
-                                static constexpr PitchType E_DoubleSharp = 24u;
-                                static constexpr PitchType E_TripleSharp = 25u;
-                                static constexpr PitchType E_QuadrupleSharp = 26u;
+                                static constexpr Type E_QuadrupleFlat = 18u;
+                                static constexpr Type E_TripleFlat = 19u;
+                                static constexpr Type E_DoubleFlat = 20u;
+                                static constexpr Type E_Flat = 21u;
+                                static constexpr Type E_Natural = 22u;
+                                static constexpr Type E_Sharp = 23u;
+                                static constexpr Type E_DoubleSharp = 24u;
+                                static constexpr Type E_TripleSharp = 25u;
+                                static constexpr Type E_QuadrupleSharp = 26u;
 
-                                static constexpr PitchType F_QuadrupleFlat = 27u;
-                                static constexpr PitchType F_TripleFlat = 28u;
-                                static constexpr PitchType F_DoubleFlat = 29u;
-                                static constexpr PitchType F_Flat = 30u;
-                                static constexpr PitchType F_Natural = 31u;
-                                static constexpr PitchType F_Sharp = 32u;
-                                static constexpr PitchType F_DoubleSharp = 33u;
-                                static constexpr PitchType F_TripleSharp = 34u;
-                                static constexpr PitchType F_QuadrupleSharp = 35u;
+                                static constexpr Type F_QuadrupleFlat = 27u;
+                                static constexpr Type F_TripleFlat = 28u;
+                                static constexpr Type F_DoubleFlat = 29u;
+                                static constexpr Type F_Flat = 30u;
+                                static constexpr Type F_Natural = 31u;
+                                static constexpr Type F_Sharp = 32u;
+                                static constexpr Type F_DoubleSharp = 33u;
+                                static constexpr Type F_TripleSharp = 34u;
+                                static constexpr Type F_QuadrupleSharp = 35u;
 
-                                static constexpr PitchType G_QuadrupleFlat = 36u;
-                                static constexpr PitchType G_TripleFlat = 37u;
-                                static constexpr PitchType G_DoubleFlat = 38u;
-                                static constexpr PitchType G_Flat = 39u;
-                                static constexpr PitchType G_Natural = 40u;
-                                static constexpr PitchType G_Sharp = 41u;
-                                static constexpr PitchType G_DoubleSharp = 42u;
-                                static constexpr PitchType G_TripleSharp = 43u;
-                                static constexpr PitchType G_QuadrupleSharp = 44u;
+                                static constexpr Type G_QuadrupleFlat = 36u;
+                                static constexpr Type G_TripleFlat = 37u;
+                                static constexpr Type G_DoubleFlat = 38u;
+                                static constexpr Type G_Flat = 39u;
+                                static constexpr Type G_Natural = 40u;
+                                static constexpr Type G_Sharp = 41u;
+                                static constexpr Type G_DoubleSharp = 42u;
+                                static constexpr Type G_TripleSharp = 43u;
+                                static constexpr Type G_QuadrupleSharp = 44u;
 
-                                static constexpr PitchType A_QuadrupleFlat = 45u;
-                                static constexpr PitchType A_TripleFlat = 46u;
-                                static constexpr PitchType A_DoubleFlat = 47u;
-                                static constexpr PitchType A_Flat = 48u;
-                                static constexpr PitchType A_Natural = 49u;
-                                static constexpr PitchType A_Sharp = 50u;
-                                static constexpr PitchType A_DoubleSharp = 51u;
-                                static constexpr PitchType A_TripleSharp = 52u;
-                                static constexpr PitchType A_QuadrupleSharp = 53u;
+                                static constexpr Type A_QuadrupleFlat = 45u;
+                                static constexpr Type A_TripleFlat = 46u;
+                                static constexpr Type A_DoubleFlat = 47u;
+                                static constexpr Type A_Flat = 48u;
+                                static constexpr Type A_Natural = 49u;
+                                static constexpr Type A_Sharp = 50u;
+                                static constexpr Type A_DoubleSharp = 51u;
+                                static constexpr Type A_TripleSharp = 52u;
+                                static constexpr Type A_QuadrupleSharp = 53u;
 
-                                static constexpr PitchType B_QuadrupleFlat = 54u;
-                                static constexpr PitchType B_TripleFlat = 55u;
-                                static constexpr PitchType B_DoubleFlat = 56u;
-                                static constexpr PitchType B_Flat = 57u;
-                                static constexpr PitchType B_Natural = 58u;
-                                static constexpr PitchType B_Sharp = 59u;
-                                static constexpr PitchType B_DoubleSharp = 60u;
-                                static constexpr PitchType B_TripleSharp = 61u;
-                                static constexpr PitchType B_QuadrupleSharp = 62u;
+                                static constexpr Type B_QuadrupleFlat = 54u;
+                                static constexpr Type B_TripleFlat = 55u;
+                                static constexpr Type B_DoubleFlat = 56u;
+                                static constexpr Type B_Flat = 57u;
+                                static constexpr Type B_Natural = 58u;
+                                static constexpr Type B_Sharp = 59u;
+                                static constexpr Type B_DoubleSharp = 60u;
+                                static constexpr Type B_TripleSharp = 61u;
+                                static constexpr Type B_QuadrupleSharp = 62u;
 
-                                static constexpr PitchType InvaildPitch = 63u;
+                                static constexpr Type InvaildPitch = 63u;
                                 static constexpr uint8_t MinIterator = 0u;
                                 static constexpr uint8_t MaxIterator = 63u;
                         //
@@ -295,8 +295,8 @@
                             public:
                                 
                                 Pitch();
-                                Pitch(PitchType token);
-                                Pitch& operator=(PitchType token);
+                                Pitch(Type token);
+                                Pitch& operator=(Type token);
                                 
                                 Pitch(const Letter& note, const Accidental& accidental);
                                 
@@ -331,8 +331,8 @@
                                 const uint8_t NotePitch() const;
                                 const uint8_t MidiPitch() const;
 
-                                const PitchType& ID() const;
-                                operator const PitchType() const;
+                                const Type& ID() const;
+                                operator const Type() const;
                                 
                                 bool Sharp();
                                 bool Flat();
@@ -350,8 +350,8 @@
                     bool operator > (const Pitch& lhs, const Pitch& rhs);
                     bool operator >= (const Pitch& lhs, const Pitch& rhs);
                     bool operator == (const Pitch& lhs, const Pitch& rhs);
-                    bool operator == (const Pitch::PitchType& lhs, const Pitch& rhs);
-                    bool operator == (const Pitch& lhs, const Pitch::PitchType& rhs);
+                    bool operator == (const Pitch::Type& lhs, const Pitch& rhs);
+                    bool operator == (const Pitch& lhs, const Pitch::Type& rhs);
                     bool operator != (const Pitch& lhs, const Pitch& rhs);
 
 
