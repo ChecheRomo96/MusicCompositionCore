4f88357a38a6facc382e3464cb55370f67132fd6
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-18 22:53:24 -0600

    Update TextFormat.h

--- src/Core/MusicalCore/MusicalNote/TextFormat/TextFormat.h
+++ src/Core/MusicalCore/MusicalNote/TextFormat/TextFormat.h
@@ -6,7 +6,6 @@
 	namespace MusicCompositionCore::Core::MusicalCore::MusicalNote::TextFormat{}
 	namespace MCC_MusicalNoteFormat = MusicCompositionCore::Core::MusicalCore::MusicalNote::TextFormat;
 
-	#include "TextFormat_Definitions.h"
 	#include "TextFormat_Class.h"
 	
 #endif//MCC_NOTE_FORMAT_H
