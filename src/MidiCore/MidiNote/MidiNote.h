58a4028e94ae960589237652ae521dcbbd5a09fc
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-20 00:14:50 -0600

    Update MidiNote.h

--- src/MidiCore/MidiNote/MidiNote.h
+++ src/MidiCore/MidiNote/MidiNote.h
@@ -2,7 +2,7 @@
 #define MCC_MIDI_NOTE_H
 
 #include <MCC_BuildSettings.h>
-#include "../MidiDefinitions.h"
+#include "../MidiProtocol.h"
 #include "../../MusicalCore/MusicalNote/MusicalNote.h"
 
 namespace MusicCompositionCore::MidiCore
