7eda7f3a24f3684e23414112bef8efdd10873b68
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-21 14:59:55 -0600

    Update MusicalScale_Definitions.h

--- src/MusicalCore/MusicalScale/MusicalScale_Definitions.h
+++ src/MusicalCore/MusicalScale/MusicalScale_Definitions.h
@@ -45,37 +45,34 @@
 
     namespace MusicCompositionCore
     {
-        namespace Core
+        namespace MusicalCore
         {
-            namespace MusicalCore
+            namespace MusicalScale
             {
-                namespace MusicalScale
+                namespace Constants
                 {
-                    namespace Constants
+                    
+                    /*static constexpr uint8_t ScaleRootsSize = 17;
+                    const MCC_MusicalNote::NoteToken ScaleRoots[ScaleRootsSize]  PROGMEM_MACRO=
                     {
-                        
-                        /*static constexpr uint8_t ScaleRootsSize = 17;
-                        const MCC_MusicalNote::NoteToken ScaleRoots[ScaleRootsSize]  PROGMEM_MACRO=
-                        {
-                            MCC_MusicalNote::Constants::NoteToken::C_Natural,
-                            MCC_MusicalNote::Constants::NoteToken::C_Sharp,
-                            MCC_MusicalNote::Constants::NoteToken::D_Flat,
-                            MCC_MusicalNote::Constants::NoteToken::D_Natural,
-                            MCC_MusicalNote::Constants::NoteToken::D_Sharp,
-                            MCC_MusicalNote::Constants::NoteToken::E_Flat,
-                            MCC_MusicalNote::Constants::NoteToken::E_Natural,
-                            MCC_MusicalNote::Constants::NoteToken::F_Natural,
-                            MCC_MusicalNote::Constants::NoteToken::F_Sharp,
-                            MCC_MusicalNote::Constants::NoteToken::G_Flat,
-                            MCC_MusicalNote::Constants::NoteToken::G_Natural,
-                            MCC_MusicalNote::Constants::NoteToken::G_Sharp,
-                            MCC_MusicalNote::Constants::NoteToken::A_Flat,
-                            MCC_MusicalNote::Constants::NoteToken::A_Natural,
-                            MCC_MusicalNote::Constants::NoteToken::A_Sharp,
-                            MCC_MusicalNote::Constants::NoteToken::B_Flat,
-                            MCC_MusicalNote::Constants::NoteToken::B_Natural,
-                        };*/
-                    }
+                        MCC_MusicalNote::Constants::NoteToken::C_Natural,
+                        MCC_MusicalNote::Constants::NoteToken::C_Sharp,
+                        MCC_MusicalNote::Constants::NoteToken::D_Flat,
+                        MCC_MusicalNote::Constants::NoteToken::D_Natural,
+                        MCC_MusicalNote::Constants::NoteToken::D_Sharp,
+                        MCC_MusicalNote::Constants::NoteToken::E_Flat,
+                        MCC_MusicalNote::Constants::NoteToken::E_Natural,
+                        MCC_MusicalNote::Constants::NoteToken::F_Natural,
+                        MCC_MusicalNote::Constants::NoteToken::F_Sharp,
+                        MCC_MusicalNote::Constants::NoteToken::G_Flat,
+                        MCC_MusicalNote::Constants::NoteToken::G_Natural,
+                        MCC_MusicalNote::Constants::NoteToken::G_Sharp,
+                        MCC_MusicalNote::Constants::NoteToken::A_Flat,
+                        MCC_MusicalNote::Constants::NoteToken::A_Natural,
+                        MCC_MusicalNote::Constants::NoteToken::A_Sharp,
+                        MCC_MusicalNote::Constants::NoteToken::B_Flat,
+                        MCC_MusicalNote::Constants::NoteToken::B_Natural,
+                    };*/
                 }
             }
         }
