577b6bb1dc241aa108a545c8a378adb4944342bb
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-21 16:09:51 -0600

    Update MCC_UserSetup.h

--- src/MCC_UserSetup.h
+++ src/MCC_UserSetup.h
@@ -8,13 +8,13 @@
             // Core
 
                 #define MCC_MUSICAL_CORE_ENABLED
-                    #define MCC_MUSICAL_NOTE_ENABLED
-                    #define MCC_MUSICAL_INTERVAL_ENABLED
-                    #define MCC_MUSICAL_SCALE_ENABLED
+                    //#define MCC_MUSICAL_NOTE_ENABLED
+                    //#define MCC_MUSICAL_INTERVAL_ENABLED
+                    //#define MCC_MUSICAL_SCALE_ENABLED
 
-                #define MCC_MIDI_CORE_ENABLED
-                    #define MCC_MIDI_NOTE_ENABLED
-                    #define MCC_MIDI_MESSAGE_ENABLED
+                //#define MCC_MIDI_CORE_ENABLED
+                    //#define MCC_MIDI_NOTE_ENABLED
+                    //#define MCC_MIDI_MESSAGE_ENABLED
             //
             //////////////////////////////////////////////////////////////
         //              
