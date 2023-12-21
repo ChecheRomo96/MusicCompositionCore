97ed3ed6b5cf197d83ac95dd27ce7aca6995c75d
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-21 14:44:02 -0600

    Update Interval_Class.h

--- src/MusicalCore/MusicalInterval/Interval_Class.h
+++ src/MusicalCore/MusicalInterval/Interval_Class.h
@@ -8,73 +8,67 @@
 
     #include "../MusicalNote/MusicalNote.h"
 
-    namespace MusicCompositionCore
+    namespace MusicCompositionCore::MusicalCore
     {
-        namespace Core
+        namespace MusicalNote
         {
-            namespace MusicalCore
-            {
-                namespace MusicalNote
-                {
-                    class Note;
-                }
-                namespace MusicalInterval
-                {
-                    class Interval
-                    { 
-                        NumberType _Number;
-                        QualityType _Quality;
+            class Note;
+        }
+        namespace MusicalInterval
+        {
+            class Interval
+            { 
+                NumberType _Number;
+                QualityType _Quality;
 
-                        public:
+                public:
 
-                            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                            // Constructors and Assignment operators
+                    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // Constructors and Assignment operators
 
-                                Interval();
-                                
-                                Interval(const QualityType& quality, const NumberType& number);
-                                Interval(int16_t _NotePitch_A, int16_t _NotePitch_B, uint8_t _GenericInterval);
-
-                                Interval(const MCC_MusicalNote::Note &source_a, const MCC_MusicalNote::Note &source_b);
-                                Interval(const MCC_MusicalNote::Pitch& source_a, const MCC_MusicalNote::Pitch& source_b);
-
-                                Interval(const Interval& src);
-                                Interval& operator=(const Interval& src);
-                            //
-                            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                            // API
-
-                                const char* Name() const;
-                                char* Name(char* buff) const;
-
-                                const uint8_t Semitones() const;
-                                Interval Inverse() const;
-
-                                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                // Generic Interval
-
-                                    void SetNumber(NumberType id);
-                                    const NumberType Number() const;
-                                //
-                                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                                // Interval Quality
-
-                                    void SetQuality(QualityType id);
-                                    const QualityType Quality() const;
-                                //
-                                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                            //
-                            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                            
+                        Interval();
                         
-                        private:
+                        Interval(const QualityType& quality, const NumberType& number);
+                        Interval(int16_t _NotePitch_A, int16_t _NotePitch_B, uint8_t _GenericInterval);
 
-                            cpstd::vector<Interval> PossibleIntervals(int16_t _NotePitch_A, int16_t _NotePitch_B);
-                    };
+                        Interval(const MCC_MusicalNote::Note &source_a, const MCC_MusicalNote::Note &source_b);
+                        Interval(const MCC_MusicalNote::Pitch& source_a, const MCC_MusicalNote::Pitch& source_b);
 
-                    extern Interval IntervalBuffer;
-                }
-            }
+                        Interval(const Interval& src);
+                        Interval& operator=(const Interval& src);
+                    //
+                    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // API
+
+                        const char* Name() const;
+                        char* Name(char* buff) const;
+
+                        const uint8_t Semitones() const;
+                        Interval Inverse() const;
+
+                        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                        // Generic Interval
+
+                            void SetNumber(NumberType id);
+                            const NumberType Number() const;
+                        //
+                        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                        // Interval Quality
+
+                            void SetQuality(QualityType id);
+                            const QualityType Quality() const;
+                        //
+                        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    //
+                    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    
+                
+                private:
+
+                    cpstd::vector<Interval> PossibleIntervals(int16_t _NotePitch_A, int16_t _NotePitch_B);
+            };
+
+            extern Interval IntervalBuffer;
         }
     }
 
