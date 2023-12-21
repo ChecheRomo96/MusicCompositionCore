bf2b865cd9707b10b32184f7b69190e2ef6511f2
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-21 15:02:59 -0600

    Update MusicalScale_Definitions.h

--- src/MusicalCore/MusicalScale/MusicalScale_Definitions.h
+++ src/MusicalCore/MusicalScale/MusicalScale_Definitions.h
@@ -3,45 +3,7 @@
 
     #include <MCC_BuildSettings.h>
 
-    #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-
-        #include "..\MusicalNote\MusicalNote.h"
-
-        #ifdef _WIN64
-          //define something for Windows (64-bit only)
-        #else
-          //define something for Windows (32-bit only)
-        #endif
-
-    #elif __APPLE__
-
-        #include <TargetConditionals.h>
-
-        #include "../MusicalNote/MusicalNote.h"
-
-        #if TARGET_IPHONE_SIMULATOR
-             // iOS Simulator
-        #elif TARGET_OS_IPHONE
-            // iOS device
-        #elif TARGET_OS_MAC
-            // Other kinds of Mac OS
-        #else
-        #   error "Unknown Apple platform"
-        #endif
-
-    #elif ARDUINO
-
-        #include "../MusicalNote/MusicalNote.h"
-
-    #elif __linux__
-        // linux
-    #elif __unix__ // all unices not caught above
-        // Unix
-    #elif defined(_POSIX_VERSION)
-        // POSIX
-    #else
-    #   error "Unknown compiler"
-    #endif
+    #include "../MusicalNote/MusicalNote.h"
 
     namespace MusicCompositionCore
     {
