e4d80d4e386423b8be68a0c3da795554f11b5245
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-21 14:55:35 -0600

    Update ScaleProperties_Class.h

--- src/MusicalCore/MusicalScale/ScaleProperties/ScaleProperties_Class.h
+++ src/MusicalCore/MusicalScale/ScaleProperties/ScaleProperties_Class.h
@@ -9,24 +9,24 @@
         {
             namespace MusicalScale
             {
-                class PropertiesContainer
-                {
+                class PropertiesContainer{
+                protected:
                     uint8_t Data;
 
-                        public:
+                public:
 
-                            PropertiesContainer(bool note_mode, uint8_t scale_location, bool name_flag);
-                            PropertiesContainer(uint8_t bitmap);
-                            PropertiesContainer(const PropertiesContainer& source);
+                    PropertiesContainer(bool note_mode, uint8_t scale_location, bool name_flag);
+                    PropertiesContainer(uint8_t bitmap);
+                    PropertiesContainer(const PropertiesContainer& source);
 
-                            const bool MappingMode() const;
-                            void SetMappingMode(bool note_mode);
-                            
-                            const uint8_t Location() const; 
-                            void SetLocation(uint8_t scale_location);
-                            
-                            const bool NameFlag() const; 
-                            void SetNameFlag(bool name_flag);
+                    const bool MappingMode() const;
+                    void SetMappingMode(bool note_mode);
+                    
+                    const uint8_t Location() const; 
+                    void SetLocation(uint8_t scale_location);
+                    
+                    const bool NameFlag() const; 
+                    void SetNameFlag(bool name_flag);
                 };
             }
         }
