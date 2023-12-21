a400d19c48321f2d94a90d4f046020f53bd073bf
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-12-21 14:54:33 -0600

    Update ScaleFlash.h

--- src/MusicalCore/MusicalScale/ScaleFlash/ScaleFlash.h
+++ src/MusicalCore/MusicalScale/ScaleFlash/ScaleFlash.h
@@ -9,82 +9,81 @@
     #include "ScaleDefinitions/ScaleDefinitions.h"
 
     namespace MusicCompositionCore::MusicalCore
+    {
+        namespace MusicalScale
         {
-            namespace MusicalScale
+            namespace Flash
             {
-                namespace Flash
-                {
-                    extern char Buffer[23];
-                    
-                    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    // Name
+                extern char Buffer[23];
+                
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Name
 
-                        ////////////////////////////////////////////////////////////////////////////////////////////////////
-                        // Fills char Buffer[23] and returns a pointer to it
+                    ////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // Fills char Buffer[23] and returns a pointer to it
 
-                            const char* GetScaleName(const MCC_MusicalNote::Pitch& root, uint8_t ScaleID);
-                            const char* GetScaleName(const Container_Notes& source);
-                            const char* GetScaleName(const Container_Mapping& source);
-                            const char* GetScaleName(const Container& source);
-                            const char* GetScaleName(const Container* source, uint8_t ID);
-                            const char* GetScaleName(uint8_t ID);
-                        //
-                        ////////////////////////////////////////////////////////////////////////////////////////////////////
-                        // Fills buff and returns a pointer to it
-
-                            char* GetScaleName(char* buff, const MCC_MusicalNote::Pitch& root, uint8_t ScaleID);
-                            char* GetScaleName(char* buff, const Container_Notes& source);
-                            char* GetScaleName(char* buff, const Container_Mapping& source);
-                            char* GetScaleName(char* buff, const Container& source);
-                            char* GetScaleName(char* buff, const Container* source, uint8_t ID);
-                            char* GetScaleName(char* buff, uint8_t ID);
-                        //
-                        ////////////////////////////////////////////////////////////////////////////////////////////////////
+                        const char* GetScaleName(const MCC_MusicalNote::Pitch& root, uint8_t ScaleID);
+                        const char* GetScaleName(const Container_Notes& source);
+                        const char* GetScaleName(const Container_Mapping& source);
+                        const char* GetScaleName(const Container& source);
+                        const char* GetScaleName(const Container* source, uint8_t ID);
+                        const char* GetScaleName(uint8_t ID);
                     //
-                    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    // ID
+                    ////////////////////////////////////////////////////////////////////////////////////////////////////
+                    // Fills buff and returns a pointer to it
 
-                        uint8_t GetScaleID(const Container_Notes& source);
-                        uint8_t GetScaleID(const Container_Mapping& source);
-                        uint8_t GetScaleID(const Container& source);
-                        uint8_t GetScaleID(const Container* source, uint8_t ID);
+                        char* GetScaleName(char* buff, const MCC_MusicalNote::Pitch& root, uint8_t ScaleID);
+                        char* GetScaleName(char* buff, const Container_Notes& source);
+                        char* GetScaleName(char* buff, const Container_Mapping& source);
+                        char* GetScaleName(char* buff, const Container& source);
+                        char* GetScaleName(char* buff, const Container* source, uint8_t ID);
+                        char* GetScaleName(char* buff, uint8_t ID);
                     //
-                    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    // Properties
+                    ////////////////////////////////////////////////////////////////////////////////////////////////////
+                //
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // ID
 
-                        uint8_t GetScaleProperties(const Container_Notes& source);
-                        uint8_t GetScaleProperties(const Container_Mapping& source);
-                        uint8_t GetScaleProperties(const Container& source);
-                        uint8_t GetScaleProperties(const Container* container, uint8_t ID);
-                        uint8_t GetScaleProperties(uint8_t ID);
-                    //
-                    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    // Mapping
+                    uint8_t GetScaleID(const Container_Notes& source);
+                    uint8_t GetScaleID(const Container_Mapping& source);
+                    uint8_t GetScaleID(const Container& source);
+                    uint8_t GetScaleID(const Container* source, uint8_t ID);
+                //
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Properties
 
-                        uint16_t GetScaleMapping(const Container_Mapping& source);
-                        uint16_t GetScaleMapping(const Container& source);
-                        uint16_t GetScaleMapping(const Container* source, uint8_t ID);
-                        uint16_t GetScaleMapping(uint8_t ID);
-                    //
-                    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    // Generic Interval
+                    uint8_t GetScaleProperties(const Container_Notes& source);
+                    uint8_t GetScaleProperties(const Container_Mapping& source);
+                    uint8_t GetScaleProperties(const Container& source);
+                    uint8_t GetScaleProperties(const Container* container, uint8_t ID);
+                    uint8_t GetScaleProperties(uint8_t ID);
+                //
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Mapping
 
-                        uint8_t GetScaleGenericInterval(const Container_Mapping& source, uint8_t x);
-                        uint8_t GetScaleGenericInterval(const Container& source, uint8_t x);
-                        uint8_t GetScaleGenericInterval(const Container* source, uint8_t ID, uint8_t x);
-                        uint8_t GetScaleGenericInterval(uint8_t ID, uint8_t x);
-                    //
-                    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    // Note Token
+                    uint16_t GetScaleMapping(const Container_Mapping& source);
+                    uint16_t GetScaleMapping(const Container& source);
+                    uint16_t GetScaleMapping(const Container* source, uint8_t ID);
+                    uint16_t GetScaleMapping(uint8_t ID);
+                //
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Generic Interval
 
-                        uint8_t GetScaleNoteToken(const Container_Notes& source, uint8_t x);
-                        uint8_t GetScaleNoteToken(const Container& source, uint8_t x);
-                        uint8_t GetScaleNoteToken(const Container* source, uint8_t ID, uint8_t x);
-                        uint8_t GetScaleNoteToken(uint8_t ID, uint8_t x);
-                    //
-                    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    uint8_t GetScaleGenericInterval(const Container_Mapping& source, uint8_t x);
+                    uint8_t GetScaleGenericInterval(const Container& source, uint8_t x);
+                    uint8_t GetScaleGenericInterval(const Container* source, uint8_t ID, uint8_t x);
+                    uint8_t GetScaleGenericInterval(uint8_t ID, uint8_t x);
+                //
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                // Note Token
 
-                }
+                    uint8_t GetScaleNoteToken(const Container_Notes& source, uint8_t x);
+                    uint8_t GetScaleNoteToken(const Container& source, uint8_t x);
+                    uint8_t GetScaleNoteToken(const Container* source, uint8_t ID, uint8_t x);
+                    uint8_t GetScaleNoteToken(uint8_t ID, uint8_t x);
+                //
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
             }
         }
     }
